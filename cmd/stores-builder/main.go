3fe24e7184a738c92e396636d8f9572cb64407da
Tim Burks <timburks@me.com>
2024-06-20 17:37:25 -0700

    evaluating tidwall/rtree

--- cmd/stores-builder/main.go
+++ cmd/stores-builder/main.go
@@ -1,31 +1,127 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
+	"io"
 	"log"
-	"net"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/bobadojo/go/pkg/stores/v1/storespb"
-	"google.golang.org/grpc"
+	"github.com/tidwall/rtree"
+	"google.golang.org/protobuf/proto"
 )
 
-type storesServer struct {
-	storespb.UnimplementedStoresServer
+func main() {
+	if err := run(); err != nil {
+		log.Fatalf("%s", err)
+	}
 }
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+func run() error {
+	var stores []*storespb.Store
+	c := 0
+	startTime := time.Now()
+	for i := 0; i < 3000; i++ {
+		filename := "data/us_post_offices_01-27-23-sample.csv"
+		file, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		stores = nil
+		reader := csv.NewReader(file)
+		reader.Read()
+		for {
+			fields, err := reader.Read()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				return err
+			}
+			store := &storespb.Store{
+				Name:  fields[0],
+				Type:  fields[1],
+				Title: fields[2],
+				Location: &storespb.Location{
+					Latitude:  atof(fields[10]),
+					Longitude: atof(fields[11]),
+				},
+				Address: &storespb.Address{
+					Street:     fields[3],
+					City:       fields[4],
+					State:      fields[5],
+					ZipCode:    atoi(fields[6]),
+					RegionCode: fields[13],
+					County:     fields[14],
+				},
+				StoreHours: fields[9],
+			}
+			stores = append(stores, store)
+			c++
+		}
 	}
-	lis, err := net.Listen("tcp", ":"+port)
+	executionTime := time.Since(startTime)
+	fmt.Printf("load of %d records ran in %+v\n", c, executionTime)
+
+	b, err := proto.Marshal(&storespb.ListStoresResponse{Stores: stores})
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return err
 	}
-	log.Printf("listening on port %s", port)
-	grpcServer := grpc.NewServer()
-	storespb.RegisterStoresServer(grpcServer, &storesServer{})
-	if err = grpcServer.Serve(lis); err != nil {
-		log.Fatal(err)
+	err = os.WriteFile("stores.pb", b, 0644)
+	if err != nil {
+		return err
 	}
+
+	var tr rtree.RTree
+	for _, store := range stores {
+		for i := 0; i < 100000; i++ {
+			point := [2]float64{
+				float64(store.Location.Latitude) + float64(i)/10000.0,
+				float64(store.Location.Longitude) + float64(i)/10000.0,
+			}
+			tr.Insert(point, point, store.Name+fmt.Sprintf("_%d", i))
+		}
+	}
+
+	log.Printf("%+v", tr.Len())
+
+	point := [2]float64{
+		38.08973633943899,
+		-96.8860452622175,
+	}
+	min := point
+	max := point
+	epsilon := 0.001
+	min[0] -= epsilon
+	min[1] -= epsilon
+	max[0] += epsilon
+	max[1] += epsilon
+
+	startTime = time.Now()
+	tr.Search(min, max, func(min, max [2]float64, data interface{}) bool {
+		log.Printf("%+v", data)
+		return true
+	})
+	executionTime = time.Since(startTime)
+	fmt.Printf("query ran in %+v\n", executionTime)
+
+	return nil
+}
+
+func atoi(s string) int32 {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	return int32(i)
+}
+
+func atof(s string) float32 {
+	f, err := strconv.ParseFloat(s, 32)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	return float32(f)
 }
--- go.mod
+++ go.mod
@@ -5,13 +5,15 @@ go 1.22.3
 require (
 	github.com/bobadojo/go v0.0.1
 	google.golang.org/grpc v1.64.0
+	google.golang.org/protobuf v1.34.2
 )
 
 require (
+	github.com/tidwall/geoindex v1.7.0 // indirect
+	github.com/tidwall/rtree v1.10.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240617180043-68d350f18fd4 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240617180043-68d350f18fd4 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
 )
